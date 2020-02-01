25647aa6c11444eb162471bc74dd12776692144c
Allen Zhao <azhao@asuconsulting.com>
2020-02-01 22:12:02 +0800

    customized to private registry

--- build16.sh
+++ build16.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOCAL_HUB="hub.nxtele.xyz"
+
 docker build -t freeswitch-docker:1.6-latest . -f Dockerfile-1.6
 
 FS_16_TMP=$(docker run --rm --entrypoint="" -it freeswitch-docker:1.6-latest /usr/bin/freeswitch -version 2> /dev/null)
@@ -12,19 +14,16 @@ FS_16_A=(${FS_16_VERSION//./ })
 FS_16_A=(${FS_16_A[@]})
 FS_16_MM="${FS_16_A[0]}.${FS_16_A[1]}"
 
-docker tag freeswitch-docker:1.6-latest readytalk/freeswitch-docker:${FS_16_VERSION}
-docker tag freeswitch-docker:1.6-latest readytalk/freeswitch-docker:${FS_16_MM}
-docker tag freeswitch-docker:1.6-latest readytalk/freeswitch-docker:1.6-latest
+docker tag freeswitch-docker:1.6-latest ${LOCAL_HUB}/asu/freeswitch-docker:${FS_16_VERSION}
+docker tag freeswitch-docker:1.6-latest ${LOCAL_HUB}/asu/freeswitch-docker:${FS_16_MM}
+docker tag freeswitch-docker:1.6-latest ${LOCAL_HUB}/asu/freeswitch-docker:1.6-latest
 
 echo "-----------------------"
-echo "Saved Tag \"readytalk/freeswitch-docker:${FS_16_VERSION}\""
-echo "Saved Tag \"readytalk/freeswitch-docker:${FS_16_MM}\""
+echo "Saved Tag \"${LOCAL_HUB}/freeswitch-docker:${FS_16_VERSION}\""
+echo "Saved Tag \"${LOCAL_HUB}/freeswitch-docker:${FS_16_MM}\""
 echo "-----------------------"
 
-if [[ ${TRAVIS} && "${TRAVIS_BRANCH}" == "master" && -n $DOCKER_USERNAME && -n $DOCKER_PASSWORD ]]; then
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push readytalk/freeswitch-docker:${FS_16_VERSION}
-  docker push readytalk/freeswitch-docker:${FS_16_MM}
-  docker push readytalk/freeswitch-docker:1.6-latest
-fi
+docker push ${LOCAL_HUB}/asu/freeswitch-docker:${FS_16_VERSION}
+docker push ${LOCAL_HUB}/asu/freeswitch-docker:${FS_16_MM}
+docker push ${LOCAL_HUB}/asu/freeswitch-docker:1.6-latest
 
